dc5af72e7b90d9abd1b9525ca9ca73d0cbd22515
Daniel Larsen <44644910+daniel-larsen@users.noreply.github.com>
2023-02-06 16:30:34 -0400

    Cleaned up code a bit.

--- build.rs
+++ build.rs
@@ -2,37 +2,25 @@ use std::process::Command;
 
 fn main() {
     println!("cargo:rerun-if-changed=package.json");
-    
+
     let js = "js/msal-browser.js";
     let js_map = format!("{}.map", js);
     std::fs::remove_file(js).unwrap_or(());
     std::fs::remove_file(js_map).unwrap_or(());
 
     if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(["/C", "npm install"])
-            .output()
-            .expect("failed to execute process")
+        Command::new("cmd").args(["/C", "npm install"]).output()
     } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg("npm install")
-            .output()
-            .expect("failed to execute process")
-    };
+        Command::new("sh").args(["-c", "npm install"]).output()
+    }
+    .unwrap();
 
     let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-            .args(["/C", "npm run build"])
-            .output()
-            .expect("failed to execute process")
+        Command::new("cmd").args(["/C", "npm run build"]).output()
     } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg("npm run build")
-            .output()
-            .expect("failed to execute process")
-    };
+        Command::new("sh").args(["-c", "npm run build"]).output()
+    }
+    .unwrap();
 
     // https://nodejs.org/api/process.html#process_exit_codes
     if output.status.code() != Some(0) {
