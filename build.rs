fce5b0f835d86b384386cab0c6ad60334e9652a5
Daniel Larsen <44644910+daniel-larsen@users.noreply.github.com>
2023-02-08 23:44:26 -0400

    build.rs improvements (#1)
    * Try with different build cmds
    
    * install packages
    
    * Cleaned up code a bit.
    
    * Tidy up code to work on windows
    
    ---------
    
    Co-authored-by: Paul Dorehill <pauldorehill@gmail.com>

--- build.rs
+++ build.rs
@@ -1,14 +1,27 @@
 use std::process::Command;
 
+fn new_npm() -> Command {
+    const NPM: &str = "npm";
+    if cfg!(windows) {
+        let mut cmd = Command::new("cmd");
+        cmd.args(["/c", NPM]);
+        cmd
+    } else {
+        Command::new(NPM)
+    }
+}
+
 fn main() {
     println!("cargo:rerun-if-changed=package.json");
-    
+
     let js = "js/msal-browser.js";
     let js_map = format!("{}.map", js);
     std::fs::remove_file(js).unwrap_or(());
     std::fs::remove_file(js_map).unwrap_or(());
 
-    let output = Command::new("npm").args(["run", "build"]).output().unwrap();
+    new_npm().arg("install").spawn().unwrap().wait().unwrap();
+    let output = new_npm().args(["run", "build"]).output().unwrap();
+
     // https://nodejs.org/api/process.html#process_exit_codes
     if output.status.code() != Some(0) {
         panic!(
